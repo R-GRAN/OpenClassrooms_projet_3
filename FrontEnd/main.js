0bb325472c01edf8655cc24883fc84e88a65759f
Remy <remy.grangenois@gmail.com>
2023-11-21 15:13:45 +0100

    filtre par catégorie ok

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,12 +1,10 @@
-const fetchData = async () => {
+async function fetchData() {
   const response = await fetch("http://localhost:5678/api/works");
   const data = await response.json();
-  
-  genererProjets(data)
+  return data;
+}
 
-};
-
-const genererProjets = (projets) => {
+async function genererProjets(projets) {
   for (let i = 0; i < projets.length; i++) {
     const projet = projets[i];
 
@@ -23,6 +21,40 @@ const genererProjets = (projets) => {
     figure.appendChild(imageElement);
     figure.appendChild(figcaptionElement);
   }
-};
+}
 
-fetchData()
+async function initialiserPage() {
+  let projets = await fetchData();
+  genererProjets(projets);
+}
+
+function resetGallery() {
+  document.querySelector(".gallery").innerHTML = "";
+}
+
+async function filtrerProjets(evt) {
+  let projets = await fetchData();
+
+  if (evt.target.value === "Tous") {
+    resetGallery();
+    genererProjets(projets);
+  } else {
+    resetGallery();
+    const projetsFiltres = projets.filter(
+      (projet) => projet.category.name === evt.target.value
+    );
+    genererProjets(projetsFiltres);
+  }
+}
+
+const btns = document.querySelectorAll(".btn-filters");
+
+btns.forEach((element) =>
+  element.addEventListener("click", (evt) => {
+    filtrerProjets(evt);
+    document.querySelector(".active").classList.remove("active");
+    element.classList.add("active");
+  })
+);
+
+initialiserPage();
